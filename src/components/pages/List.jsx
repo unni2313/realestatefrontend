e50c9f6c2c060755e94e24192ece08a404f596ec
unni2313 <felixsebastian2026@mca.ajce.in>
2025-03-27 18:06:56 +0530

    changed the link 7

--- src/components/pages/List.jsx
+++ src/components/pages/List.jsx
@@ -10,7 +10,7 @@ const List = () => {
 
     const fetchProperties = async () => {
         try {
-            
+
             const response = await axios.get("https://realestatebackend-uund.onrender.com/intemInserting");
             setProperties(response.data);
         } catch (error) {
